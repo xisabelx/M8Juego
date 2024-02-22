e4344930f144a2b6b382fa2aad12caf767b7084b
isa <isa.pask@gmail.com>
2024-02-22 19:04:11 +0100

    FIN FASE4

--- app/src/main/java/com/example/m8juego/Menu.kt
+++ app/src/main/java/com/example/m8juego/Menu.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Menu : AppCompatActivity() {
     //creem unes variables per comprovar ususari i authentificació
@@ -52,6 +58,7 @@ class Menu : AppCompatActivity() {
         CreditsBtn.setTypeface(tf)
         PuntuacionsBtn.setTypeface(tf)
         jugarBtn.setTypeface(tf)
+        consulta()
 
         tancarSessio.setOnClickListener(){
             tancalaSessio()
@@ -93,6 +100,46 @@ class Menu : AppCompatActivity() {
         val intent= Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
+    }
+    private fun consulta(){
+        var database: FirebaseDatabase = FirebaseDatabase.getInstance("https://m8juego-e9538-default-rtdb.europe-west1.firebasedatabase.app/")
+        var bdreference:DatabaseReference = database.getReference("DATA BASE JUGADORS")
+        bdreference.addValueEventListener (object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.i ("DEBUG","arrel value"+
+                        snapshot.getValue().toString())
+                Log.i ("DEBUG","arrel key"+ snapshot.key.toString())
+                // ara capturem tots els fills
+                var trobat:Boolean =false
+                for (ds in snapshot.getChildren()) {
+                    Log.i ("DEBUG","DS key: "+ds.child("Uid").key.toString())
+                    Log.i ("DEBUG","DS value: "+ds.child("Uid").getValue().toString())
+                    Log.i ("DEBUG","DS data:  "+ds.child("Data").getValue().toString())
+                    Log.i ("DEBUG","DS mail:  "+ds.child("Email").getValue().toString())
+                    //mirem si el mail és el mateix que el del jugador
+                    //si és així, mostrem les dades als textview corresponents
+                        if (ds.child("Email").getValue().toString().equals(user?.email)){
+                            trobat=true
+                            //carrega els textview
+                            puntuacio.setText(
+                                ds.child("Puntuacio").getValue().toString())
+                            uid.setText(
+                                ds.child("Uid").getValue().toString())
+                            correo.setText(
+                                ds.child("Email").getValue().toString())
+                            nom.setText(
+                                ds.child("Nom").getValue().toString())
+                        }
+                        if (!trobat)
+                        {
+                            Log.e ("ERROR","ERROR NO TROBAT MAIL")
+                        }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.e ("ERROR","ERROR DATABASE CANCEL")
+            }
+        })
     }
 
 
